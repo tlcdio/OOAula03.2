d285ac9bee08db98c1d307dabb2abc35f87a4885
thiagoleitedio@gmail.com <thiagoleitedio@gmail.com>
2021-11-09 12:48:52 -0300

    share

---
+++ src/one/digitalinnovation/oo/Carro.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.oo;
+
+/**
+ * Classe de exemplo para o exercício da Aula 2 de Orientação a Objetos.
+ */
+class Carro {
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+
+}
